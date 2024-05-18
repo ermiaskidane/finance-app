05f962fb46f1f6876f39ba0ffc87f62e3a3b9b67
ermias kidane <ermiaskidanegebre@gmail.com>
2024-05-18 19:30:30 +0100

    ci: integration of csv react-papaparse package

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -20,14 +20,38 @@ import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete-a
 import { useBulkCreateTransactions } from "@/features/transactions/api/use-bulk-create-transactions";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
+import { useState } from "react";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
 
+enum VARIANTS {
+  LIST = "LIST",
+  IMPORT = "IMPORT"
+};
+
+const INITIAL_IMPORT_RESULTS = { data: [], errors: [], meta: {} };
 
 const TransactionsPage = () => {
+  const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+  const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+  const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+    console.log(results)
+    setImportResults(results);
+    setVariant(VARIANTS.IMPORT);
+  };
+
+  const onCancelImport = () => {
+    setImportResults(INITIAL_IMPORT_RESULTS);
+    setVariant(VARIANTS.LIST);
+  };
+  
   const { onOpen } = useNewTransaction();
   const bulkCreateMutation = useBulkCreateTransactions();
   const bulkDeleteMutation = useBulkDeleteTransactions();
   const transactionsQuery = useGetTransactions();
   const transactionsData = transactionsQuery.data || [];
+
 
   const isDisabled =
     transactionsQuery.isLoading || 
@@ -50,6 +74,20 @@ const TransactionsPage = () => {
     );
   }
 
+  if (variant === VARIANTS.IMPORT) {
+    return (
+      <>
+      <h1>this is for UploadButton</h1>
+        {/* <AccountDialog /> */}
+        <ImportCard
+          data={importResults.data}
+          onCancel={onCancelImport}
+          onSubmit={() => {}}
+        />
+      </>
+    );
+  }
+
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -57,10 +95,13 @@ const TransactionsPage = () => {
         <CardTitle className="text-xl line-clamp-1">
           Transaction History
         </CardTitle>
-        <Button onClick={onOpen} size="sm" className="w-full lg:w-auto">
-          <Plus className="size-4 mr-2" />
-          Add new
-        </Button>
+        <div className="flex items-center gap-x-2">
+          <Button onClick={onOpen} size="sm" className="w-full lg:w-auto">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+          <UploadButton onUpload={onUpload} />
+        </div>
       </CardHeader>
       <CardContent>
         <DataTable
